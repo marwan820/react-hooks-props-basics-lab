be8e060cb7582dbed0d2f08eb5d4af2e0e56dd3f
Marwan Rahim <marwan820@Marwan>
2023-08-01 18:26:14 -0500

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,14 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About 
-      github = "https://github.com/liza" 
-      linkedin = "https://www.linkedin.com/in/liza/" />
+      <Home 
+      name={user.name} city={user.city} color={user.color}
+      />
+      <About
+      bio = {user.bio}
+      github = {user.links.github}
+      linkedin = {user.links.linkedin}
+      />
     
       
     </div>
